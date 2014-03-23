91cce787bdf04ffc31e29c702e09f343c7a950e3
Hotlib <jackiechanaaa@gmail.com>
2014-03-23 17:15:01 +0100

    adding project description info

--- zugradar/project.clj
+++ zugradar/project.clj
@@ -1,8 +1,8 @@
 (defproject zugradar "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "ZugRadar Train Status"
+  :url "https://github.com/hotlib/"
+  :license {:name "MIT License"
+            :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [clj-http-lite "0.2.0"]
                  [compojure "1.1.6"]
